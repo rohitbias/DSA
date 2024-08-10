a587982b2098ef3a355ad860da2269c4d24863df
Rohit Bhatt <rohit.bias@gmail.com>
2024-08-10 13:35:29 -0500

    UpdatedCode

--- ContainsDuplicates.java
+++ ContainsDuplicates.java
@@ -13,31 +13,40 @@ Output: false
 
 */
 
-
-
 import java.util.*;
 
-class ContainsDuplicates{
-    public static void main(String args[])
-    {
-        int[] nums = {1, 2, 3, 4,1};
-        int[] nums1 = {1,1, 2, 3, 3};
-        System.out.println(hasDuplicates(nums));
-        System.out.println(hasDuplicates(nums1));
+class ContainsDuplicates {
+    public static void main(String args[]) {
+        int[] nums = { 1, 24, 3, 4, 1 };
+        String[] names = { "Rohit", "mayank", "Ram", "Shyam" };
+        System.out.println(hasDuplicatesNumber(nums));
+        System.out.println(hasDuplicateString(names));
+
     }
 
-
-    public static boolean hasDuplicates(int [] nums)
-    {
-        HashSet<Integer> hs= new HashSet<Integer>();
-        for(int i=0;i<nums.length;i++)
-        {
-                if(hs.contains(nums[i]))
-                {
+    public static boolean hasDuplicatesNumber(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hs.contains(nums[i])) {
                 return true;
             }
-        hs.add(nums[i]);
+            hs.add(nums[i]);
         }
         return false;
-    }      
+    }
+
+    public static boolean hasDuplicateString(String[] name) {
+        HashSet<String> names = new HashSet<String>();
+        for (int i = 0; i < name.length; i++) {
+            if (names.contains(name[i])) {
+                return true;
+            }
+            names.add(name[i]);
+        }
+
+        return false;
+    }
 }
+
+// Time Complexity is : O(n) ---> where n is lenght of the array/String passed
+// and it takes O(1) to insert or access any element in an HashSet
