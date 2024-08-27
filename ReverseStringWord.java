4f3b2e2e414554e839ec79a9fe5787291f44d4f6
Rohit Bhatt <rohit.bias@gmail.com>
2024-08-27 13:07:56 -0500

    updated with better approach
     Note: Time complexity of concoatening a string (Output+=str[i]) is O(N) where
    // as Time complexity of appending a string is o(1)

--- ReverseStringWord.java
+++ ReverseStringWord.java
@@ -8,16 +8,20 @@
 public class ReverseStringWord {
 
     public static void main(String[] args) {
-        System.err.println(reverseString("this   is a   cat"));
+        System.err.println(reverseString("   this   is a   cat    "));
     }
 
     public static String reverseString(String s) {
-        String[] str = s.split("\\s+"); // this will split the string based on space
-        String output = "";
+        String[] str = s.trim().split("\\s+"); // this will split the string based on space.
+        // Time complexity of trim and split function is O(N)
+        StringBuilder output = new StringBuilder();
         for (int i = str.length - 1; i >= 0; i--) {
-            output = output + str[i] + " ";
-        }
-        return output.trim();
-    }
+            output.append(str[i]);
+            output.append(' ');
 
+        }
+        return output.toString();
+    }
 }
+// Note: Time complexity of concoatening a string (Output+=str[i]) is O(N) where
+// as Time complexity of appending a string is o(1)
